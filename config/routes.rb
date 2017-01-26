bc29a0276d2561cb943372c95cc5a2ca4f9dc981
Boh <bohlok@gmx.de>
2017-01-26 17:37:30 +0000

    Home and Session Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/home'
+
+  get '/login', to: 'session#new'
+  get '/signup', to: 'visitors#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root to: 'visitors#new'
+ 
   resources :visitors 
 end
